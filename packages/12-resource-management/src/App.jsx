5afe5b93c3cbe098675f8c693099075862b0466b
lena-don <lena08don@gmail.com>
2026-08-27 10:49:47 +0300

    12ыйурок

--- packages/12-resource-management/src/App.jsx
+++ packages/12-resource-management/src/App.jsx
@@ -1,8 +1,88 @@
+import { Component, useEffect, useState } from 'react'
+
+
+// 1. Class Component
+// Управление ресурсом через componentDidMount /
+// componentWillUnmount
+
+class Clock extends Component {
+
+  constructor(props) {
+    super(props)
+
+    // Начальное состояние — текущая дата и время:
+    this.state = {
+      date: new Date()
+    }
+
+    // Привязываем метод к экземпляру компонента:
+    this.unmount = this.unmount.bind(this)
+  }
+
+
+  // Удаляем компонент.
+  // Это приведёт к вызову componentWillUnmount() ! Сделать проверку через логи там, где нужна наглядность жизненного цикла компонента (из урока 11)
+  unmount() {
+    this.unmount()
+  }
+
+
+  // Компонент уже добавлен в DOM
+  // Здесь создаём ресурс часы
+  componentDidMount() {
+
+    this.timerId = setInterval(
+      () => this.tick(),
+      1000
+    )
+
+    console.log('componentDidMount()')
+  }
+
+
+  // Компонент удаляется из DOM.
+  // Здесь обязательно освобождаем созданный ресурс.
+  componentWillUnmount() {
+
+    clearInterval(this.timerId)
+
+    console.log('componentWillUnmount()')
+  }
+
+
+  // Каждую секунду обновляем state.
+  tick() {
+
+    this.setState({
+      date: new Date()
+    })
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        <h2>
+          Текущее время:{' '}
+          {this.state.date.toLocaleTimeString()}
+        </h2>
+      </div>
+    )
+  }
+}
+
 function App() {
+  const [show, setShow] = useState(true)
+
   return (
-    <main>
-      <h1>12-resource-management</h1>
-    </main>
+    <>
+      {show && <Clock />}
+
+      <button onClick={() => setShow(false)}>
+        Удалить часики
+      </button>
+    </>
   )
 }
 

3661712b02c88a006fca622c2fef98614ce02555
lena-don <lena08don@gmail.com>
2026-08-27 10:59:20 +0300

    12урок wip

--- packages/12-resource-management/src/App.jsx
+++ packages/12-resource-management/src/App.jsx
@@ -10,25 +10,25 @@ class Clock extends Component {
   constructor(props) {
     super(props)
 
-    // Начальное состояние — текущая дата и время:
+    // Начальное состояние — текущая дата и время.
     this.state = {
       date: new Date()
     }
 
-    // Привязываем метод к экземпляру компонента:
+    // Привязываем метод к экземпляру компонента.
     this.unmount = this.unmount.bind(this)
   }
 
 
   // Удаляем компонент.
-  // Это приведёт к вызову componentWillUnmount() ! Сделать проверку через логи там, где нужна наглядность жизненного цикла компонента (из урока 11)
+  // Это приведёт к вызову componentWillUnmount()? Сделать проверку через логи там, где нужна наглядность жизненного цикла компонента (из урока 11)
   unmount() {
     this.unmount()
   }
 
 
-  // Компонент уже добавлен в DOM
-  // Здесь создаём ресурс часы
+  // Компонент уже добавлен в DOM.
+  // Здесь создаём ресурс — часы.
   componentDidMount() {
 
     this.timerId = setInterval(
@@ -67,23 +67,34 @@ class Clock extends Component {
           Текущее время:{' '}
           {this.state.date.toLocaleTimeString()}
         </h2>
+
+        <button onClick={this.unmount}> 
+          {/* Адаптировать под мой проект с шаблоном от Vite */}
+          Удалить компонент
+        </button>
       </div>
     )
   }
 }
 
 function App() {
-  const [show, setShow] = useState(true) // Это отдельно разобрать и вынести,
-  // в учебных материалах сделано по-другому, так как там другая структура проекта,
-  // а у меня на базе стандартного Реакт-приложения из Вита.
 
   return (
     <>
-      {show && <Clock />}
+      <section>
 
-      <button onClick={() => setShow(false)}>
-        Удалить часики
-      </button>
+        <h2>
+          1. Class Component
+        </h2>
+
+        <p>
+          Часы создаются в componentDidMount(),
+          а удаляются в componentWillUnmount().
+        </p>
+
+        <Clock />
+
+      </section>
     </>
   )
 }

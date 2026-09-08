60423faa27b681624e976d44a6bc096dfecda426
lena-don <lena08don@gmail.com>
2026-09-08 09:25:10 +0300

    19wip

--- packages/19-working-with-form-elements/src/App.jsx
+++ packages/19-working-with-form-elements/src/App.jsx
@@ -1,9 +1,90 @@
-function App() {
+import { useState } from "react";
+import React from "react";
+
+// 1. Чекбокс
+
+function Terms() {
+  // Состояние чекбокса false = не отмечен
+  const [isChecked, setIsChecked] = useState(false);
+
+  // Меняем состояние на противоположное !
+  const handleChange = () => {
+    setIsChecked((prev) => !prev);
+  };
+
   return (
-    <main>
-      <h1>19-working-with-form-elements</h1>
-    </main>
-  )
+    <section>
+      <h2>1. Чекбокс</h2>
+
+      <label>
+        Принять условия:
+      </label>
+
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleChange}
+      />
+
+      <p>
+        {isChecked
+          ? "Условия приняты"
+          : "Необходимо принять условия"}
+      </p>
+    </section>
+  );
 }
 
-export default App
+
+// 2. Чекбокс в классовом компоненте
+class ClassTerms extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isChecked: false,
+    };
+  }
+
+  handleChange = () => {
+    this.setState({
+      isChecked: !this.state.isChecked,
+    });
+  };
+
+  render() {
+    return (
+      <section>
+        <h2>2. Чекбокс в классовом компоненте</h2>
+
+        <label>
+          Принять условия:
+        </label>
+
+        <input
+          type="checkbox"
+          checked={this.state.isChecked}
+          onChange={this.handleChange}
+        />
+
+        <p>
+          {this.state.isChecked
+            ? "Условия приняты"
+            : "Необходимо принять условия"}
+        </p>
+      </section>
+    );
+  }
+}
+
+export default function App() {
+  return (
+    <main>
+
+      <Terms />
+
+      <ClassTerms />
+
+    </main>
+  );
+}
